f5f15d143a36ba1092c47ba19780b1162d9b0817
Ueli Kunz <kunz@ideadapt.net>
2021-12-03 21:46:25 +0100

    make simple DateServer capable of handling multiple clients at once, repeatedly.

--- src/main/java/ch/ibw/clientServer/server/dateReply/DateServer.java
+++ src/main/java/ch/ibw/clientServer/server/dateReply/DateServer.java
@@ -1,9 +1,12 @@
 package ch.ibw.clientServer.server.dateReply;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
 import java.util.Date;
 
 /**
@@ -15,13 +18,39 @@ import java.util.Date;
  * @see <https://cs.lmu.edu/~ray/notes/javanetexamples/>
  */
 public class DateServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket listener = new ServerSocket(6060)) {
-            System.out.println("DateServer läuft");
-            try (Socket socket = listener.accept()) {   // Warte auf Clientverbindung
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(new Date().toString());     // Sende Antwort an Client
+  public static void main(String[] args) throws IOException {
+    try (ServerSocket listener = new ServerSocket(6060)) { // Port reservieren
+      System.out.println("DateServer läuft");
+
+      while (true) {
+        Socket socket = listener.accept();
+        new Thread(() -> {
+          try {
+            PrintWriter zumClient = new PrintWriter(socket.getOutputStream(), true);
+            zumClient.println("Datumsformat?");
+
+            BufferedReader vomClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String dateFormat = vomClient.readLine();
+
+            String date = "unbekanntes Format";
+            if (dateFormat.equalsIgnoreCase("date")) {
+              date = new Date().toString();
+            } else if (dateFormat.equalsIgnoreCase("time")) {
+              date = DateFormat.getTimeInstance().format(new Date());
             }
-        }
+
+            zumClient.println(date);
+          } catch (IOException e) {
+            e.printStackTrace();
+          } finally {
+            try {
+              socket.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+        }).start();
+      }
     }
+  }
 }
